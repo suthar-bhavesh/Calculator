6cb9a3f181e4211c3e14489a0d8eef50b151302a
Bhavesh Suthar <officialbhaveshsuthar>
2025-10-05 23:52:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+let Input = document.getElementById('calc-input');
+let buttons = document.querySelectorAll('.btn-calc');
+
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const value = button.textContent
+
+        if (value === "AC") {
+            Input.value = "";
+        }
+        else if (value === "DEL") {
+            Input.value = Input.value.slice(0, -1)
+        }
+        else if (value === "=") {
+            let UserInput = Input.value;
+            const operators = ['+', '*', '-', '/'];
+
+            let userchar = UserInput.slice(-1);
+
+            if (operators.includes(userchar)) {
+                UserInput = UserInput.slice(0, -1);
+            }
+
+            try {
+                if (UserInput.includes('%')) {
+                    UserInput = UserInput.replace(/%/g, '/100');
+                }
+                Input.value = eval(UserInput)
+            } catch {
+                alert('Please Enter Valid Digit');
+            }
+        } else {
+            Input.value += value;
+        }
+    });
+});
